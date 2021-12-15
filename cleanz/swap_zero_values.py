ab74808f224ea9a0b93e0a8ff2f16f251e068119
albint3r <albint3r@gmail.com>
2021-12-14 18:00:15 -0600

    Fist Test: test_swap_zero_m2

--- cleanz/swap_zero_values.py
+++ cleanz/swap_zero_values.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def swap_zerovalues_to_mean(dataframe: pd.DataFrame, column_name: str, min_search_value: int = 0,
                             type_of_listing_val: str = None, alternative_value_complete: str = None,
-                            print_msg_results: bool = True) -> None:
+                            print_msg_results: bool = True) -> int:
     """ Swap the zero values for the mean of the area
 
     Select a Column name and add the minimum values you want to change for the mean in the area.
@@ -37,12 +37,11 @@ def swap_zerovalues_to_mean(dataframe: pd.DataFrame, column_name: str, min_searc
 
     Returns
     ---------
-    None
+    int
     """
 
     # Select the Zero Values Column
     zero_values_column = dataframe[column_name] <= min_search_value
-    star_total_zero_values = zero_values_column.sum()
 
     # Only used if is necessary to add a type of listing for the subset
     if type_of_listing_val is not None:
@@ -50,25 +49,36 @@ def swap_zerovalues_to_mean(dataframe: pd.DataFrame, column_name: str, min_searc
 
         # Only apply this subset if is TRUE
         list_zone_zero_vals = dataframe.loc[(zero_values_column & type_of_listing), 'colonia'].unique()
+        # Count total of Missing Values: -> n
+        star_total_zero_values = (zero_values_column & type_of_listing).sum()
+        doble_subset = True
     else:
         # This is list creator of the name of the zones that doesn't have the value
         list_zone_zero_vals = dataframe.loc[zero_values_column, 'colonia'].unique()
+        # Count total of Missing Values: -> n
+        star_total_zero_values = zero_values_column.sum()
+        doble_subset = False
 
     for zone_name in list_zone_zero_vals:
 
         # Set the values to subset
         zone = dataframe['colonia'] == zone_name
-        zone_value_not_null = dataframe[column_name] != min_search_value
+        zone_value_not_null = dataframe[column_name] > min_search_value
 
         # Calculate the mean of the columns that have values
         mean_value = np.mean(dataframe.loc[(zone & zone_value_not_null), column_name])
 
-        try:
-            dataframe.loc[(zone & zero_values_column), column_name] = round(mean_value)
-            msg = f'{zone_name}:..........{mean_value:.1f} m2'
-        except ValueError:
+        # Change NaN values to zero = 0
+        if pd.isna(mean_value):
+            mean_value = 0
 
-            # Helps to use another value and not Zero
+        try:
+            # This is the expected Path
+            dataframe.loc[(zone & zero_values_column), column_name] = np.round(mean_value)  # <- Round Result
+            msg = f'{zone_name}:..........{mean_value:.1f} m2'
+
+        except ValueError:
+            # This is the path if the row is don't have more info to create the mean.
             if alternative_value_complete is not None:
                 alternative_value = dataframe.loc[zone, alternative_value_complete]  # <- Select the alternative value
                 dataframe.loc[(zone & zero_values_column), column_name] = alternative_value
@@ -81,7 +91,16 @@ def swap_zerovalues_to_mean(dataframe: pd.DataFrame, column_name: str, min_searc
         if print_msg_results:
             print(msg)
 
-    end_total_zero_values = (dataframe[column_name] == min_search_value).sum()
+    # Calculate again the 0 Values remaining in the DataFrame
+    corroborate_zero_val = dataframe[column_name] <= min_search_value # <- Corroborate the Cleaning Rows
+
+    # Count the Missing Data Again
+    if doble_subset:
+        end_total_zero_values = (corroborate_zero_val & type_of_listing).sum()
+    else:
+        end_total_zero_values = corroborate_zero_val.sum()
+
+    # Subtract the initial zero values with the result
     total_values_corrected = star_total_zero_values - end_total_zero_values
 
     if print_msg_results:
@@ -89,3 +108,5 @@ def swap_zerovalues_to_mean(dataframe: pd.DataFrame, column_name: str, min_searc
         print(f'\nStart total Zero Values: {star_total_zero_values}')
         print(f'End total Zero Values: {end_total_zero_values}')
         print(f'Total Corrected Data: {total_values_corrected}')
+
+    return end_total_zero_values
--- tests/test_swap.py
+++ tests/test_swap.py
@@ -1,26 +1,28 @@
 import pytest
 from cleanz.swap_zero_values import swap_zerovalues_to_mean
 import pandas as pd
-import numpy as np
+
 
 class TestSwapZeroValues(object):
     """Test the function Swap_Zero_Values"""
 
     @pytest.fixture
-    def setup_df(self):
-        data = {'colonia': ['Puerta de Hierro', 'Puerta de Hierro', 'Puerta de Hierro', 'Chapalita', 'Chapalita',
-                            'Chapalita',
-                            'Valle', 'Valle', 'zero_1', 'zero_2'],
-                'm2_terreno': [200, 210, 0, 200, 210, 0, 250, 0, 0, 0],
-                'm2_const': [220, 230, 0, 220, 230, 0, 240, 0, 0, 0],
-                'habitaciones': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0],
-                'banos': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0],
-                'autos': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0],
-                'tipo_inmueble': ['Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa']}
+    def setup_df(self) -> pd.DataFrame:
+        """Create the initial DataFrame for the test"""
+
+        data = {'colonia': ['Puerta de Hierro', 'Puerta de Hierro', 'Puerta de Hierro', 'Loma Larga', 'Loma Larga',
+                            'Loma Larga', 'Valle', 'Valle', 'zero_1', 'zero_2', 'Legacy', 'Legacy', 'Legacy', ],
+                'm2_terreno': [500, 510, 0, 200, 210, 0, 250, 0, 0, 100, 0, 0, 0],
+                'm2_const': [220, 230, 0, 220, 230, 0, 240, 0, 0, 0, 85, 85, 0],
+                'habitaciones': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0, 2, 2, 0],
+                'banos': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 2],
+                'autos': [3, 3, 0, 2, 2, 0, 2, 0, 0, 0, 2, 2, 0],
+                'tipo_inmueble': ['Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa', 'Casa',
+                                  'Casa', 'Departamento', 'Departamento', 'Departamento']}
 
         return pd.DataFrame(data)
 
-    def test_swap_zero_m2(self, setup_df):
+    def test_swap_zero_m2(self, setup_df: pd.DataFrame):
         """Test the m2 complete works correctly
 
         Parameters
@@ -28,10 +30,44 @@ class TestSwapZeroValues(object):
         setup_df: pandas.DataFrame
             Is the test tabular data for the tests.
         """
-
+        # Create DataFrame to test
         df = setup_df
-        actual = df.loc[2, 'm2_terreno']
-        expected = 200.5
-        swap_zerovalues_to_mean(df, 'm2_terreno', min_search_value=10, type_of_listing_val='Casa')
 
-        assert actual == expected, f'Expected Value: {expected} and the actual.....{actual}'
+        # Total Zero Remaining Results expected : 1
+        actual = swap_zerovalues_to_mean(df, 'm2_terreno', min_search_value=10, type_of_listing_val='Casa')
+        expected = 1
+        msg = f'Expected Value:...{expected} and the actual Result was:...{actual}'
+
+        assert actual == expected, msg
+
+        # Expected results : Puerta de Hiero
+        name1 = df.loc[2, 'colonia']
+        actual1 = df.loc[2, 'm2_terreno']
+        expected1 = 505
+        msg1 = f'Expected Value in {name1}:...{expected1} and the actual Result was:...{actual1}'
+
+        assert actual1 == expected1, msg1
+
+        # Expected results : Loma Larga
+        name2 = df.loc[5, 'colonia']
+        actual2 = df.loc[5, 'm2_terreno']
+        expected2 = 205
+        msg2 = f'Expected Value in {name2}:...{expected2} and the actual Result was:...{actual2}'
+
+        assert actual2 == expected2, msg2
+
+        # Expected results : Valle
+        name3 = df.loc[7, 'colonia']
+        actual3 = df.loc[7, 'm2_terreno']
+        expected3 = 250
+        msg3 = f'Expected Value in {name3}:...{expected3} and the actual Result was:...{actual3}'
+
+        assert actual3 == expected3, msg3
+
+        # Expected results : Zero1
+        name4 = df.loc[8, 'colonia']
+        actual4 = df.loc[8, 'm2_terreno']
+        expected4 = 0
+        msg4 = f'Expected Value in {name4}:...{expected4} and the actual Result was:...{actual4}'
+
+        assert actual4 == expected4, msg4
