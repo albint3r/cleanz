d0d8329d3bd61cd3fa98f26c1c13888eb2386192
albint3r <albint3r@gmail.com>
2021-12-16 13:02:35 -0600

    Add the Count drop zero func

--- cleanz/quality_data_selector.py
+++ cleanz/quality_data_selector.py
@@ -2,7 +2,35 @@ import pandas as pd
 import numpy as np
 
 
-def quality_data_check(dataframe: pd.DataFrame, target_cols: list) -> pd.DataFrame:
+def count_drop_zero(dataframe: pd.DataFrame, target_cols: list) -> tuple[int, int]:
+    """Count the posible drop values if apply the apply de replace drop zero
+
+    Parameters
+    -----------
+    dataframe: pd.DataFrame:
+        This is the dataframe to drop the columns
+
+    target_cols: list:
+        This is the list of the columns you want to know how many rows will be drop
+        if you apply the NaN drop.
+
+    Returns
+    ----------
+    Int
+    """
+    start_total_vals = len(dataframe)
+    # Convert Zero to NaN to then drop it
+    copy_df = dataframe[target_cols].replace(0, np.nan)
+    count_drop_nan = len(copy_df[target_cols].dropna(how='all'))
+    result = start_total_vals - count_drop_nan
+
+    print(f'Total rows DataFrame:...{start_total_vals}')
+    print(f'Total rows remaining:...{count_drop_nan}')
+    print(f'Total rows drop it:...{result}')
+
+    return result, count_drop_nan
+
+def replace_drop_zero(dataframe: pd.DataFrame, target_cols: list) -> pd.DataFrame:
     """Check the quality of the final data Swap Zero Values
 
     First change the 0 values for Nan Values. Then it would drop it the row that have
@@ -24,9 +52,9 @@ def quality_data_check(dataframe: pd.DataFrame, target_cols: list) -> pd.DataFra
     """
 
     # Convert Zero to NaN to then drop it
-    copy_df = dataframe[target_cols].replace(0, np.nan)
+    dataframe[target_cols] = dataframe[target_cols].replace(0, np.nan)
     # Drop NaN Values
-    result = copy_df[target_cols].dropna(how='all')
+    result = dataframe[target_cols].dropna(how='all')
 
     return result
 
--- tests/test_quality_data_selector.py
+++ tests/test_quality_data_selector.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from cleanz.quality_data_selector import quality_data_check
+from cleanz.quality_data_selector import replace_drop_zero, count_drop_zero
 
 
 class TestQualityData(object):
@@ -21,23 +21,44 @@ class TestQualityData(object):
 
         return pd.DataFrame(data)
 
-    def test_quality_data_selector(self, setup_df: pd.DataFrame):
-        """Test the fucntion Quality Data Selector"""
+    def test_count_drop_zero_result(self, setup_df: pd.DataFrame):
+        """Test the function drop zero to identify the total row it will drop if apply the function"""
 
         df = setup_df
 
-        actual = len(quality_data_check(df, ['m2_const', 'm2_terreno']))
+        actual, _ = count_drop_zero(df, ['m2_const', 'm2_terreno'])
+        expected = 5  # <- Rows
+        msg = f'The expected result is: {expected} and you have  {actual}'
+        assert actual == expected, msg
+
+    def test_count_drop_zero_count(self, setup_df: pd.DataFrame):
+        """Test the function drop zero to identify the total row it will keep"""
+
+        df = setup_df
+
+        _, actual = count_drop_zero(df, ['m2_const', 'm2_terreno'])
         expected = 8  # <- Rows
         msg = f'The expected result is: {expected} and you have  {actual}'
         assert actual == expected, msg
 
-    def test_quality_data_selector_diff(self, setup_df: pd.DataFrame):
+    def test_replace_drop_zero(self, setup_df: pd.DataFrame):
+        """Test the fucntion Quality Data Selector"""
+
+        df = setup_df
+
+        actual = len(replace_drop_zero(df, ['m2_const', 'm2_terreno']))
+        expected = 8  # <- Rows
+        msg = f'The expected result is: {expected} and you have  {actual}'
+        assert actual == expected, msg
+
+    def test_replace_drop_zero_diff(self, setup_df: pd.DataFrame):
         """Test the diferencen between the initial data and the final result"""
 
         df = setup_df
         initial_len_df = len(df)
 
-        actual = initial_len_df - len(quality_data_check(df, ['m2_const', 'm2_terreno']))
+        actual = initial_len_df - len(replace_drop_zero(df, ['m2_const', 'm2_terreno']))
         expected = 5  # <- Rows
         msg = f'The expected result is: {expected} and you have  {actual}'
         assert actual == expected, msg
+
